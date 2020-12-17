ea6c9dd9610d45675dd7d5762d7ff42e4da9eb32
CSharp-Application <62175446+CSharp-Application@users.noreply.github.com>
2020-12-17 20:09:07 +0900

    Add project

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 計算問題作成アプリ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            list.Clear();
+            for (int i = 0; i < int.Parse(nanko.Value.ToString()); i++)
+            {
+                string s = "";
+                s += random.Next(0, int.Parse(mae.Value.ToString()));
+                switch (syurui.SelectedItem)
+                {
+                    case "＋":
+                        s += "＋";
+                        break;
+                    case "ー":
+                        s += "ー";
+                        break;
+                    case "×":
+                        s += "×";
+                        break;
+                    case "÷":
+                        s += "÷";
+                        break;
+                }
+                s += random.Next(0, int.Parse(ato.Value.ToString()));
+                list.Add(s);
+            }
+            kekka form = new kekka();
+            form.list = list;
+            form.ShowDialog();
+        }
+    }
+}
---
+++ kekka.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace 計算問題作成アプリ
+{
+    public partial class kekka : Form
+    {
+        public List<string> list { get; set; }
+
+        public kekka()
+        {
+            InitializeComponent();
+        }
+
+        private void kekka_Load(object sender, EventArgs e)
+        {
+            foreach (string s in list)
+            {
+                listBox1.Items.Add(s);
+            }
+        }
+    }
+}
